d5c62bce30293505b83e146defa6c2f2d75554c3
Madan Kumar <madan@kbxdigital.com>
2023-02-11 23:32:03 +0530

    donations changes

--- src/views/Donation.vue
+++ src/views/Donation.vue
@@ -2,6 +2,83 @@
     <div>
         <TopBarVue/>
         <div style="display: flex; flex-direction : column ; align-items:flex-start;padding: 3% 3%">
+            <div style="display:flex;flex-direction: row;justify-content: center; align-items: center;padding: 1% 0%;">
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>Nom de la Banque :</span>
+                </div>
+                <div style="font-size: 1.2rem;font-weight: 400;">
+                    <span>&nbsp;&nbsp;&nbsp;VISTA GUI(EX BICGUI)</span>
+                </div>
+            </div>
+            <div style="display:flex;flex-direction: row;justify-content: center; align-items: center;padding: 1% 0%;">
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>Adresse :</span>
+                </div>
+                <div style="font-size: 1.2rem;font-weight: 400;">
+                    <span>&nbsp;&nbsp;&nbsp;Avenue de la Republique, Commune de Kaloum, Conakry, BP 1484</span>
+                </div>
+            </div>
+            <div style="display:flex;flex-direction: row;justify-content: center; align-items: center;padding: 1% 0%;">
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>Intitule de compte :</span>
+                </div>
+                <div style="font-size: 1.2rem;font-weight: 400;">
+                    <span>&nbsp;&nbsp;&nbsp;M CAMARA SEKOU BENNA</span>
+                </div>
+            </div>
+            <div style="display:flex;flex-direction: row;justify-content: center; align-items: center;padding: 1% 0%;">
+                <div style="display: flex;flex-direction: column;align-items: center;justify-content: center;">
+                    <div style="font-size: 1.2rem;font-weight: 600;">
+                        <span>Code Agence</span>
+                    </div>
+                    <div style="font-size: 1.2rem;font-weight: 600;">
+                        <span>&nbsp;&nbsp;&nbsp;09842</span>
+                    </div>
+                </div>
+                <div style="display: flex;flex-direction: column;align-items: center;justify-content: center;">
+                    <div style="font-size: 1.2rem;font-weight: 600;">
+                        <span>Numero de Compte :</span>
+                    </div>
+                    <div style="font-size: 1.2rem;font-weight: 600;">
+                        <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10394701023&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;USD</span>
+                    </div>
+                </div>
+            </div>
+            <div style="display:flex;flex-direction: row;justify-content: center; align-items: center;padding: 1% 0%;">
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>Swift ou Bic :</span>
+                </div>
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>&nbsp;&nbsp;&nbsp;BICIGNCXXXX</span>
+                </div>
+            </div>
+            <div style="display:flex;flex-direction: row;justify-content: center; align-items: center;padding: 1% 0%;">
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>REFERENCE BANCAIRE COMPTE CORRESPONDANT EN USD VISTAGUI :</span>
+                </div>
+                <div style="font-size: 1.2rem;font-weight: 400;">
+                    <span>&nbsp;&nbsp;</span>
+                </div>
+            </div>
+            <div style="display:flex;flex-direction: row;justify-content: center; align-items: center;padding: 1% 0%;">
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>NOM DE LA Banque :</span>
+                </div>
+                <div style="font-size: 1.2rem;font-weight: 400;">
+                    <span>&nbsp;&nbsp;&nbsp;BIAT TUNISIE (BANQUE INTERNATIONALE ARABE DE TUNISIE)</span>
+                </div>
+            </div>
+            <div style="display:flex;flex-direction: row;justify-content: center; align-items: center;padding: 1% 0%;">
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>Code Swift ou Bic :</span>
+                </div>
+                <div style="font-size: 1.2rem;font-weight: 600;">
+                    <span>&nbsp;&nbsp;&nbsp;BIATTNTTXXXX</span>
+                </div>
+            </div>
+            <span>Nous vous en souhaitons bonne reception</span>
+        </div>
+        <!-- <div style="display: flex; flex-direction : column ; align-items:flex-start;padding: 3% 3%">
             <h3  style="margin-bottom:2%;">DEVENIR MEMBRE DU PARTI GUD</h3>
             <div style="display:flex; flex-direction :row; justify-content:space-between;">
                 <div style="display: flex;
@@ -110,7 +187,7 @@
                     </template>
                 </v-checkbox>
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
